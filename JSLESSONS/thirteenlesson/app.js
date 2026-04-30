5a6f735ed5c6d749ee22529c5b90a31933d7a6a5
unknown <dianadairbaeva@gmail.com>
2026-04-30 22:30:58 +0500

    практика 30.04.

---
+++ JSLESSONS/thirteenlesson/app.js
@@ -0,0 +1,234 @@
+const API_URL = "https://dummyjson.com"
+
+const loginScreen = document.getElementById("loginScreen")
+const profileScreen = document.getElementById("profileScreen")
+// добавить еще скрины для продуктов и одного продукта
+
+// Переменные экрана товаров
+const productsScreen = document.getElementById("productsScreen")
+const detailScreen = document.getElementById("detailScreen")
+
+// Переменные одного товара
+const productsBlock = document.getElementById("productsBlock")
+const productDetailBlock = document.getElementById("productDetailBlock")
+
+// Login переменные
+const loginForm = document.getElementById("loginForm")
+const usernameInput = document.getElementById("usernameInput")
+const passwordInput = document.getElementById("passwordInput")
+const loginMessage = document.getElementById("loginMessage")
+
+// Profile переменные
+const profileBlock = document.getElementById("profileBlock")
+const logoutBtn = document.getElementById("logoutBtn")
+
+// Кнопки
+const goToProductsBtn = document.getElementById("goToProductsBtn")
+const backtoProfileBtn = document.getElementById("backtoProfileBtn")
+const backToProductsBtn = document.getElementById("backToProductsBtn")
+
+function showScreen(screenName){
+    loginScreen.classList.add("hidden")
+    profileScreen.classList.add("hidden")
+    productsScreen.classList.add("hidden")
+    detailScreen.classList.add("hidden")
+
+    if(screenName === "login"){
+        loginScreen.classList.remove("hidden")
+        logoutBtn.classList.add("hidden")
+    }
+    if(screenName === "profile"){
+        profileScreen.classList.remove("hidden")
+        logoutBtn.classList.remove("hidden")
+    }
+    if(screenName === "products"){
+        productsScreen.classList.remove("hidden")
+        logoutBtn.classList.remove("hidden")
+    }
+    if(screenName === "detail"){
+        detailScreen.classList.remove("hidden")
+        logoutBtn.classList.remove("hidden")
+    }
+}
+
+loginForm.addEventListener("submit", async function (event){
+    event.preventDefault()
+
+    const username = usernameInput.value.trim()
+    const password = passwordInput.value.trim()
+
+    if(username === "" || password === ""){
+        loginMessage.textContent = "Введите username и password"
+        return
+    }
+
+    await loginUser(username, password)
+})
+
+async function loginUser(username,password){
+    try{
+        loginMessage.textContent = "Выполняется вход..."
+
+        const response = await fetch(`${API_URL}/auth/login`, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                expiresMins: 30
+            })
+        })
+        if(!response.ok){
+            throw new Error("Неверный логин или пароль")
+        }
+        const data = await response.json()
+
+        localStorage.setItem("accessToken", data.accessToken)
+        loginMessage.textContent = ""
+
+      await loadProfile()
+    }catch(error){
+        loginMessage.textContent = error.message
+    }
+}
+
+async function loadProfile(){
+    try{
+        profileBlock.innerHTML = "<p>Загрузка профиля...</p>"
+
+        const token = localStorage.getItem("accessToken")
+
+        if(!token){
+            showScreen("login")
+            return
+        }
+
+        const response = await fetch(`${API_URL}/auth/me`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        if(!response.ok){
+            throw new Error("Не удалось получить профиль")
+        }
+        const user = await response.json()
+
+        renderProfile(user)
+        showScreen("profile")
+    }catch(error){
+        localStorage.removeItem("accessToken")
+        loginMessage.textContent = "Сессия истекла. Войдите снова."
+        showScreen("login")
+    }
+}
+
+function renderProfile(user){
+    profileBlock.innerHTML = `
+    <img src = "${user.image}" alt = "${user.firstName}">
+
+    <div>
+        <h3>${user.firstName}</h3>
+        <p><strong>Username:</strong> ${user.username}</p>
+        <p>Email: ${user.email}</p>
+        <p>Phone: ${user.phone}</p>
+        <p>Birthday: ${user.birthDate}</p>
+        <p>Gender: ${user.gender}</p>
+    </div>
+    `
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+    const token = localStorage.getItem("accessToken")
+
+    if(token){
+        loadProfile()
+    }
+    else{
+        showScreen("login")
+    }
+})
+
+
+logoutBtn.addEventListener("click", function(){
+    localStorage.removeItem("accessToken")
+
+    usernameInput.value = ""
+    passwordInput.value = ""
+    profileBlock.innerHTML = ""
+
+    showScreen("login")
+})
+
+// 1) Кнопка перехода к экрану товаров
+goToProductsBtn.addEventListener("click", function(){
+    loadProducts()
+})
+
+// 2) Получение товаров с сервера
+async function loadProducts(){
+    try{
+        productsBlock.innerHTML = "Загрузка..."
+
+        const response = await fetch(`${API_URL}/products`)
+        const data = await response.json()
+
+        renderProducts(data.products)
+        showScreen("products")
+    }catch(e){
+        productsBlock.innerHTML = "Ошибка загрузки"
+    }
+}
+
+// 3) Render товаров
+function renderProducts(products){
+    productsBlock.innerHTML = ""
+
+    products.forEach(p => {
+        const div = document.createElement("div")
+
+        div.innerHTML = `
+            <img src="${p.thumbnail}">
+            <h3>${p.title}</h3>
+            <p>${p.description}</p>
+            <p>${p.price}$</p>
+            <p>⭐ ${p.rating}</p>
+            <button data-id="${p.id}">Подробнее</button>
+        `
+
+        div.querySelector("button").onclick = () => {
+            loadProductDetail(p.id)
+        }
+
+        productsBlock.appendChild(div)
+    })
+}
+
+// 4) Возвращение к профилю
+backtoProfileBtn.addEventListener("click", function(){
+    showScreen("profile")
+})
+
+// 5) Экран одного товара
+async function loadProductDetail(id){
+    const response = await fetch(`${API_URL}/products/${id}`)
+    const product = await response.json()
+
+    productDetailBlock.innerHTML = `
+        <img src="${product.thumbnail}">
+        <h2>${product.title}</h2>
+        <p>${product.description}</p>
+        <p>${product.price}$</p>
+        <p>⭐ ${product.rating}</p>
+    `
+
+    showScreen("detail")
+}
+
+// Возвращение обратно ко всем товарам
+backToProductsBtn.addEventListener("click", function(){
+    showScreen("products")
+})
+
--- mini-shop/app.js
+++ mini-shop/app.js
@@ -17,7 +17,7 @@ const goToRegisterBtn = document.querySelector("#goToRegisterBtn")
 
 // Переменные магазина
 const shopScreen = document.querySelector("#shopScreen")
-const userInfo = document.querySelector("#shopScreen")
+const userInfo = document.querySelector("#userInfo")
 const logoutBtn = document.querySelector("#logoutBtn")
 const productList = document.querySelector("#productList")
 const cartList = document.querySelector("#cartList")
@@ -57,7 +57,7 @@ function saveUser(user){
 }
 
 function getUser(){
-    const user = localStorage.get("user")
+    const user = localStorage.getItem("user")
 
     if (user === null){
         return null
@@ -102,6 +102,194 @@ registerForm.addEventListener("submit", (event) =>{
 
     saveUser(user)
     registerForm.reset()
-    showShopScreen()
+    showLoginScreen()
 })
 
+// Логин
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const email = loginEmail.value.trim()
+    const password = loginPassword.value.trim()
+
+    loginError.textContent = ""
+
+    const savedUser = getUser()
+
+    if (savedUser === null){
+        loginError.textContent = "Сначала нужно зарегистрироваться"
+        loginError.style.color = "red"
+        return
+    }
+
+    if (email !== savedUser.email){
+        loginError.textContent = "Неверный логин"
+        loginError.style.color = "red"
+        return
+    }
+
+    if(password !== savedUser.password){
+        loginError.textContent = "Неверный пароль"
+        loginError.style.color = "red"
+    }
+    localStorage.setItem("isAuth", "true")
+
+    loginForm.reset()
+
+    startShop()
+})
+
+// Магазин
+
+const products = [
+    {
+        id:1,
+        title: "ноутбук",
+        price: 350000
+    },
+
+    {
+        id:2,
+        title: "смартфон",
+        price: 210000
+    },
+
+    {
+        id:3,
+        title: "клавиатура",
+        price: 25000
+    }
+]
+
+// Корзина
+let cart = []
+
+// Имитация запросов к серверу через promise
+function fakeFetchProducts(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 2000)
+    })
+}
+
+// Загрузка товаров async/await
+async function loadProducts(){
+    productList.textContent = "Загрузка товаров..."
+
+    const loadedProducts = await fakeFetchProducts()
+
+    renderProducts(loadedProducts)
+}
+
+// Рендер товаров
+function renderProducts(productsArray){
+    productList.innerHTML = ""
+
+    productsArray.forEach((product) => {
+        productList.innerHTML += `
+        <div> 
+            <h4>${product.title}</h4>
+            <p>цена ${product.price} тг </p>
+            <button class = "addToCartBtn" data-id = "${product.id}">ДОбавить корзину</button>
+        </div>
+        <hr>
+        `
+    })
+}
+
+function startShop(){
+    const savedUser = getUser()
+
+    if(savedUser !== null){
+        userInfo.textContent = `Пользователь: ${savedUser.name}`
+    }
+    showShopScreen()
+    loadCartFromStorage()
+    renderCart()
+    loadProducts()
+}
+
+// Сохранение корзины в LS
+function saveCartToStorage(){
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+// Загрузка из Ls
+function loadCartFromStorage(){
+    const savedCart = localStorage.getItem("cart")
+
+    if(savedCart === null){
+        cart = []
+        return
+    }
+    cart = JSON.parse(savedCart)
+}
+
+// Добавление в корзину
+productList.addEventListener("click", (event) => {
+    if(event.target.classList.contains("addToCartBtn")){
+        const productId = Number(event.target.dataset.id)
+
+        const product = products.find((item) => {
+            return item.id === productId
+        })
+        cart.push(product)
+        saveCartToStorage()
+        renderCart()
+    }
+})
+
+// Рендер корзины
+function renderCart(){
+    cartList.innerHTML = ""
+
+    if(cart.length === 0){
+        cartList.textContent = "корзина пустая"
+        cartTotal.textContent = "итого 0 тг"
+        return
+    }
+
+    cart.forEach((product, index) => {
+        cartList.innerHTML += `
+        <div>
+            <span>${product.title} - ${product.price} тг </span>
+            <button class = "removeFromCartBtn" data-index = "${index}">удалить</button>
+        </div>
+        `
+    })
+
+    const total = cart.reduce((sum, product) => {
+        return sum + product.price
+    }, 0)
+    cartTotal.textContent = `Итого ${total} тг`
+}
+
+// Удаление
+cartList.addEventListener("click", (event) => {
+    if(event.target.classList.contains("removeFromCartBtn")){
+        const index = Number(event.target.dataset.index)
+        cart.splice(index,1)
+        saveCartToStorage()
+        renderCart()
+    }
+})
+
+// Кнопка выйти
+logoutBtn.addEventListener("click", () =>{
+    localStorage.removeItem("isAuth")
+    showLoginScreen()
+})
+
+// Проверка авторизации
+function checkAuth(){
+    const isAuth = localStorage.getItem("isAuth")
+    if(isAuth === "true"){
+        startShop()
+    }
+    else{
+        showRegisterScreen()
+    }
+}
+checkAuth()
